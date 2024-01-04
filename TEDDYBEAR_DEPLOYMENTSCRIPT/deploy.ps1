2d66e7797c4839489aa46856a96d5da5b7884ee7
Aubin Bakana <aubinbakana@hotmail.com>
2024-01-04 11:10:26 +0000

    DeploymenT script: stage a App setting for multiple apps. Gets a list of file and transform accordingly.

--- TEDDYBEAR_DEPLOYMENTSCRIPT/deploy.ps1
+++ TEDDYBEAR_DEPLOYMENTSCRIPT/deploy.ps1
@@ -1,7 +1,13 @@
+$resourceGroupName = 'learndeploymentscript_dev'
+New-AzResourceGroup -Location eastus -Name $resourceGroupName
+
 $templateFile = 'main.bicep'
+$templateParameterFile = 'azuredeploy.parameters.json'
 $today = Get-Date -Format 'MM-dd-yyyy'
 $deploymentName = "deploymentscript-$today"
+
 New-AzResourceGroupDeployment `
-  -ResourceGroupName $resourceGroupName `
-  -Name $deploymentName `
-  -TemplateFile $templateFile
+    -ResourceGroupName $resourceGroupName `
+    -Name $deploymentName `
+    -TemplateFile $templateFile `
+    -TemplateParameterFile $templateParameterFile
